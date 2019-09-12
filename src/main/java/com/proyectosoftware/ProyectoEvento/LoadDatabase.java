e6df4fca4932bbde2f46dcd72226c678f8a72ad0
Diego La Faye <lafayediego@gmail.com>
2019-09-12 15:17:04 -0400

    Creando la clase Database

---
+++ src/main/java/com/proyectosoftware/ProyectoEvento/LoadDatabase.java
@@ -0,0 +1,23 @@
+package com.proyectosoftware.ProyectoEvento;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(CulturalEventRepository repository) {
+        return args -> {
+            log.info("Preloading " + repository.save(new CulturalEvent("Feria Japonesa", "Obrajes",200,"Feria Japonesa para exponer el arte y diferentes costumbres de Japòn en Bolivia")));
+            log.info("Preloading " + repository.save(new CulturalEvent("Alasitas", "Campo Ferial",2000,"Feria de las alasitas para compartir con la familia")));
+            log.info("Preloading " + repository.save(new CulturalEvent("Feria del libro", "Campo Ferial",2000,"Feria XXI Del libro ")));
+
+
+        };
+    }
+}
