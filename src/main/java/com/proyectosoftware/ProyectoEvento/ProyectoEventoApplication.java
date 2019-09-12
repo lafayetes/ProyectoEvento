3c9814eea94afbd7083f367a23534d5e2e1fde66
Diego La Faye <lafayediego@gmail.com>
2019-09-12 14:51:02 -0400

    Initial commit

---
+++ src/main/java/com/proyectosoftware/ProyectoEvento/ProyectoEventoApplication.java
@@ -0,0 +1,13 @@
+package com.proyectosoftware.ProyectoEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEventoApplication.class, args);
+	}
+
+}
